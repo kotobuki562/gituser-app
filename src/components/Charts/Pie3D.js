2c1280edf0ae1892f9927c3d54276a36f6a34dbf
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-01-07 11:40:53 +0900

    commit

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -32,6 +32,7 @@ const ChartComponent = ({ data }) => {
         theme: "fusion",
         decimals: 0,
         pieRadius: "45%",
+        // paletteColors: "#f0db4f",
       },
       // Chart Data
       data,
