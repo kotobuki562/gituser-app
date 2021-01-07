8d90b73d7af516d1f1456be77b8b2ea25465a130
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-01-07 10:29:04 +0900

    reposData/outPut

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,6 @@ import { FiUsers, FiUserPlus } from "react-icons/fi";
 const UserInfo = () => {
   const { githubUser } = React.useContext(GithubContext);
   const { public_repos, followers, following, public_gists } = githubUser;
-  console.log(public_repos);
 
   const items = [
     {
