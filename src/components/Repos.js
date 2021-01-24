768f43fab5b9763d8e0b702c07bace308d8a43a7
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-01-24 11:55:43 +0900

    editChart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,26 +4,30 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13",
-    },
-    {
-      label: "CSS",
-      value: "160",
-    },
-    {
-      label: "JavaScript",
-      value: "80",
-    },
-  ];
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData}></Pie3D>
+        <Pie3D data={languages}></Pie3D>
         {/* <ExampleChart data={chartData} /> */}
       </Wrapper>
     </section>
